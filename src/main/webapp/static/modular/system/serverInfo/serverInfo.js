e53e66fa1b56c1f4ac033b0fe194aa1f3e2c4a31
wangshifeng <785707939@qq.com>
2018-12-27 11:03:14 +0800

    操作图表化，修复开启关闭没有刷新问题

--- src/main/webapp/static/modular/system/serverInfo/serverInfo.js
+++ src/main/webapp/static/modular/system/serverInfo/serverInfo.js
@@ -14,49 +14,58 @@ var ServerInfo = {
 ServerInfo.initColumn = function () {
     return [
         {field: 'selectItem', checkbox: true},
-            {title: '主键id', field: 'id', visible: false, align: 'center', valign: 'middle'},
-            {title: '端口名', field: 'portName', sortable: true, align: 'center', valign: 'middle',width:'7%'},
-            {title: '服务器地址', field: 'host', sortable: true, align: 'center', valign: 'middle',width:'7%'},
-            {title: '端口', field: 'port', sortable: true, align: 'center', valign: 'middle'},
-            {title: '协议', field: 'protocolName', sortable: true, align: 'center', valign: 'middle'},
-            {title: '访问端口', field: 'aliPort', sortable: true,
-                formatter: function (value, row, index) {
-                    if(row.protocolName == "http"){
-                        return "";
-                    }else{
-                        var href=row.protocolName + "://" + value;
-                        return value;
-                    }
-                },
-                align: 'center', valign: 'middle'},
-            {title: 'ngrok端口', field: 'ngrokPort', visible: false, align: 'center', valign: 'middle',width:'7%'},
-            {title: '状态', field: 'aliStatusName', sortable: true, align: 'center', valign: 'middle'},
-            {title: '访问状态', field: 'fireStatusName', visible: false, align: 'center', valign: 'middle'},
-            {title: '域名', field: 'domainName', sortable: true, align: 'center',
-                formatter: function (value, row, index) {
-                    if(row.protocolName == "http"){
-                        var href=row.protocolName + "://" + row.portName + "." + value;
-                        return "<a target='_blank' href=\""+href+"\">"+href+"</a>";
-                    }else{
-                        var href=row.protocolName + "://" + value;
-                        return href;
-                    }
-                },
-                valign: 'middle',width:'18%',},
-            {title: '区域 ', field: 'fireZone', visible: false, align: 'center', valign: 'middle'},
-            {title: '类型', field: 'typeName', sortable: true, align: 'center', valign: 'middle'},
-            {title: '备注', field: 'remark', sortable: true, align: 'center', valign: 'middle'},
-            {title: '创建时间', field: 'createTime', sortable: true, align: 'center', valign: 'middle'},
-            {title: '更新时间', field: 'updateTime', visible: false, align: 'center', valign: 'middle'},
-        {title: '操作', field: 'action', align: 'center', valign: 'middle',width:'22%',
+        {title: '主键id', field: 'id', visible: false, align: 'center', valign: 'middle'},
+        {title: '端口名', field: 'portName', sortable: true, align: 'center', valign: 'middle', width: '7%'},
+        {title: '服务器地址', field: 'host', sortable: true, align: 'center', valign: 'middle', width: '7%'},
+        {title: '端口', field: 'port', sortable: true, align: 'center', valign: 'middle'},
+        {title: '协议', field: 'protocolName', sortable: true, align: 'center', valign: 'middle'},
+        {
+            title: '访问端口', field: 'aliPort', sortable: true,
             formatter: function (value, row, index) {
-                    return "<span>" +
-                        "<div class=\"btn btn-default btn-sm edit\">修改</div>\n" +
-                        "<div class=\"btn btn-default btn-sm con\">连通测试</div>\n" +
-                        "<div class=\"btn btn-default btn-sm open\">开启代理</div>\n" +
-                        "<div class=\"btn btn-default btn-sm down\">关闭代理</div>" +
-                        "</span>"
-            },events: "action_event"
+                if (row.protocolName == "http") {
+                    return "";
+                } else {
+                    var href = row.protocolName + "://" + value;
+                    return value;
+                }
+            },
+            align: 'center', valign: 'middle'
+        },
+        {title: 'ngrok端口', field: 'ngrokPort', visible: false, align: 'center', valign: 'middle', width: '7%'},
+        {title: '状态', field: 'aliStatusName', sortable: true, align: 'center', valign: 'middle'},
+        {title: '访问状态', field: 'fireStatusName', visible: false, align: 'center', valign: 'middle'},
+        {
+            title: '域名', field: 'domainName', sortable: true, align: 'center',
+            formatter: function (value, row, index) {
+                if (row.protocolName == "http") {
+                    var href = row.protocolName + "://" + row.portName + "." + value;
+                    return "<a target='_blank' href=\"" + href + "\">" + href + "</a>";
+                } else {
+                    var href = row.protocolName + "://" + value;
+                    return href;
+                }
+            },
+            valign: 'middle', width: '18%',
+        },
+        {title: '区域 ', field: 'fireZone', visible: false, align: 'center', valign: 'middle'},
+        {title: '类型', field: 'typeName', sortable: true, align: 'center', valign: 'middle'},
+        {title: '备注', field: 'remark', sortable: true, align: 'center', valign: 'middle'},
+        {title: '创建时间', field: 'createTime', sortable: true, align: 'center', width: '12%', valign: 'middle'},
+        {title: '更新时间', field: 'updateTime', visible: false, align: 'center', width: '12%', valign: 'middle'},
+        {
+            title: '操作', field: 'action', align: 'center', valign: 'middle', width: '8%',
+            formatter: function (value, row, index) {
+                console.log(row);
+                var status = "<div class=\" down fa fa-toggle-on fa-2x text-info\"></div>\n";
+                if (row.aliStatus == 2) {
+                    status = "<div class=\"open fa fa-toggle-off fa-2x text-muted\"></div>\n";
+                }
+                return "<span>" +
+                    "<div class=\"edit fa fa-pencil-square-o fa-2x text-info\" aria-placeholder='修改'></div>\n" +
+                    "<div class=\"con fa fa-bolt fa-2x text-info\" aria-placeholder='连通测试'></div>\n" +
+                    status +
+                    "</span>"
+            }, events: "action_event"
         }
     ];
 };
@@ -67,10 +76,10 @@ action_event = {
         ServerInfo.openServerInfoDetail(row);
     },
     "click .con": function (e, value, row) {
-        if(row.protocolName=="http"){
+        if (row.protocolName == "http") {
             ServerInfo.httpConnectTest(row);
-        }else{
-            Feng.info("请采用特殊方式"+row.typeName+"测试");
+        } else {
+            Feng.info("请采用特殊方式" + row.typeName + "测试");
         }
     },
     "click .open": function (e, value, row) {
@@ -86,10 +95,10 @@ action_event = {
  */
 ServerInfo.check = function () {
     var selected = $('#' + this.id).bootstrapTable('getSelections');
-    if(selected.length == 0){
+    if (selected.length == 0) {
         Feng.info("请先选中表格中的某一记录！");
         return false;
-    }else{
+    } else {
         ServerInfo.seItem = selected[0];
         return true;
     }
@@ -115,16 +124,16 @@ ServerInfo.openAddServerInfo = function () {
  */
 ServerInfo.openServerInfoDetail = function (row) {
     //if (this.check()) {
-        var index = layer.open({
-            type: 2,
-            title: '服务器管理详情',
-            area: ['600px', '700px'], //宽高
-            fix: false, //不固定
-            maxmin: true,
-            content: Feng.ctxPath + '/serverInfo/serverInfo_update/' + row.id
-        });
-        this.layerIndex = index;
-   // }
+    var index = layer.open({
+        type: 2,
+        title: '服务器管理详情',
+        area: ['600px', '700px'], //宽高
+        fix: false, //不固定
+        maxmin: true,
+        content: Feng.ctxPath + '/serverInfo/serverInfo_update/' + row.id
+    });
+    this.layerIndex = index;
+    // }
 };
 
 /**
@@ -132,7 +141,7 @@ ServerInfo.openServerInfoDetail = function (row) {
  */
 ServerInfo.deleteSelect = function () {
     var d_list = ServerInfo.table.btInstance.bootstrapTable('getSelections');
-    for(var i in d_list) {
+    for (var i in d_list) {
         var row = d_list[i];
         ServerInfo.delete(row);
     }
@@ -143,7 +152,7 @@ ServerInfo.deleteSelect = function () {
  * 删除服务器管理
  */
 ServerInfo.delete = function (row) {
-    Feng.confirm("确定要删除"+row.port+"？",function() {
+    Feng.confirm("确定要删除" + row.port + "？", function () {
         if (this.check()) {
             var ajax = new $ax(Feng.ctxPath + "/serverInfo/delete", function (data) {
                 Feng.success("删除成功!");
@@ -157,7 +166,7 @@ ServerInfo.delete = function (row) {
     });
 };
 
-ServerInfo.connectTest=function(){
+ServerInfo.connectTest = function () {
     if (this.check()) {
         Feng.info("测试");
     }
@@ -166,15 +175,15 @@ ServerInfo.connectTest=function(){
  * http连接测试
  * @param row
  */
-ServerInfo.httpConnectTest=function(row){
+ServerInfo.httpConnectTest = function (row) {
     var ajax = new $ax(Feng.ctxPath + "/httpConnect/httpConnectTest", function (data) {
         console.info(data.description);
         console.info(data.status);
-        toastr.info(data.description.substr(0, 100),data.status);
+        toastr.info(data.description.substr(0, 100), data.status);
     }, function (data) {
         Feng.error("失败!");
     }, true);
-    ajax.set("urlStr","http://"+row.portName+"."+row.domainName+"");
+    ajax.set("urlStr", "http://" + row.portName + "." + row.domainName + "");
     ajax.start();
 }
 
@@ -183,7 +192,7 @@ ServerInfo.httpConnectTest=function(row){
  */
 ServerInfo.openSelect = function () {
     var d_list = ServerInfo.table.btInstance.bootstrapTable('getSelections');
-    for(var i in d_list) {
+    for (var i in d_list) {
         var row = d_list[i];
         ServerInfo.action(row, "start");
     }
@@ -195,7 +204,7 @@ ServerInfo.openSelect = function () {
  */
 ServerInfo.downSelect = function () {
     var d_list = ServerInfo.table.btInstance.bootstrapTable('getSelections');
-    for(var i in d_list) {
+    for (var i in d_list) {
         var row = d_list[i];
         ServerInfo.action(row, "close");
     }
@@ -205,12 +214,12 @@ ServerInfo.downSelect = function () {
 /**
  * 代理操作，开启和关闭
  */
-ServerInfo.action=function(row, action){
-    Feng.confirm("确定执行此操作："+action+"？",function(){
+ServerInfo.action = function (row, action) {
+    Feng.confirm("确定执行此操作：" + action + "？", function () {
         var ajax = new $ax(Feng.ctxPath + "/serverInfo/action", function (data) {
             console.log("操作代理返回的数据：", data);
             Feng.success("操作成功!");
-            Opinion.table.refresh();
+            ServerInfo.table.refresh();
         }, function (data) {
             Feng.error("操作失败!" + data.responseJSON.message + "!");
         }, true);
